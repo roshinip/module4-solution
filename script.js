3b5b4977387da2e0fffb8846f397420e9c1aaa2d
roshinip <47588738+roshinip@users.noreply.github.com>
2019-03-04 12:20:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 (function() {
-    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+    var names = ["Yaswanth", "John", "Jenny", "Jaggu", "Pallavi", "Falgun", "Lasya", "Pushpa", "Lavanya", "Janu"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
